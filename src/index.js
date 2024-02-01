c5518d0194ed13956466cceee1d25befe80200d3
Jaclyn Wirth <ja01wirt@siena.edu>
2024-02-01 12:19:39 -0500

    End of 2/1/24

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import "tachyons";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CardList/>
+    <App />
   </React.StrictMode>
 );
 
