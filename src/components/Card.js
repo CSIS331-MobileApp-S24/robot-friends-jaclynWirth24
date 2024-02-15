bc3bf6b5a30f13385c76f97a9f94cf0ce7edbe15
Jaclyn Wirth <ja01wirt@siena.edu>
2024-02-15 12:01:33 -0500

    Finished? 2/15/24

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ const Card = ({id, name, email}) => {
     return(
         <div className='bg-light-green dib br3 ma2 pa3 tc grow bw2 shadow-5'>
             <img 
-                src ={`https://robohash.org/${id}?size=200x200`}
+                src ={`https://robohash.org/${name}?size=200x200`}
                 alt ="pic of robot"/>
             <div>
                 <h2>{name}</h2>
